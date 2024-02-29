c1be8b284a5f7dd26f736823b63f4918a3cd3397
Andre Prieto <56053930+Andre700@users.noreply.github.com>
2024-02-29 19:37:35 -0400

    created player and added collision and movement

---
+++ scripts/player.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+@export var gravity = 400
+@export var jump_force = 200
+@export var speed = 125
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	if Input.is_action_just_pressed("jump") && is_on_floor():
+		velocity.y = -jump_force
+	
+	var direction = Input.get_axis("move_left", "move_right")
+	
+	velocity.x = direction * speed
+	
+	move_and_slide()
